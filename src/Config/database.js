23cc50ac6cca17344ea39b1a4ea1c5fa41a78c59
Mayank Pillai <mayankpillai99@gmail.com>
2025-09-20 14:32:22 +0530

    temporary change

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log(process.env.DB_CONNECTION_SECRET);
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET||"mongodb+srv://mayank:FJJm0LnxI9dXXOUa@backend.lifpaoi.mongodb.net/devTinder");
 };
 
 module.exports = connectDB;
--- src/Middlewares/auth.js
+++ src/Middlewares/auth.js
@@ -8,7 +8,7 @@ const userAuth = async (req, res, next) => {
       return res.status(401).send("Please Login!");
     }
 
-    const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
+    const decodedObj = jwt.verify(token, process.env.JWT_SECRET||"DEV@Tinder$970");
 
     const { _id } = decodedObj;
 
