ee9e4b25e1fd834336d47b8edb94baca1a1ac2cb
Mayank Pillai <mayankpillai99@gmail.com>
2025-09-20 18:03:27 +0530

    made changes to .env file

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET||"mongodb+srv://mayank:FJJm0LnxI9dXXOUa@backend.lifpaoi.mongodb.net/devTinder");
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
 module.exports = connectDB;
+ 
--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use("/", userRouter);
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(process.env.PORT||7777, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is successfully listening on port 7777...");
     });
   })
